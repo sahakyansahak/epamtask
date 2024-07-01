346e956ac3acc213bfc61da9dc66b0c4c56a6e54
gorgevorgyan253 <166944768+gorgevorgyan253@users.noreply.github.com>
2024-07-01 12:28:57 +0400

    Update variables

--- task03/main.tf
+++ task03/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_storage_account" "main" {
 
 resource "azurerm_virtual_network" "main" {
   name                = var.vnet_name
-  address_space       = [var.vnet_address]
+  address_space       = var.vnet_address
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -29,12 +29,12 @@ resource "azurerm_subnet" "public" {
   name                 = var.public_subnet_name
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [var.subnet1_address]
+  address_prefixes     = var.subnet1_address
 }
 
 resource "azurerm_subnet" "private" {
   name                 = var.private_subnet_name
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [var.subnet2_address]
+  address_prefixes     = var.subnet2_address
 }
--- task03/variables.tf
+++ task03/variables.tf
@@ -45,15 +45,15 @@ variable "tag_name" {
 
 variable "vnet_address" {
   type        = string
-  default     = "10.0.0.0/16"
+  default     = ["10.0.0.0/16"]
 }
 
 variable "subnet1_address" {
   type        = string
-  default     = "10.0.1.0/24"
+  default     = ["10.0.1.0/24"]
 }
 
 variable "subnet2_address" {
   type        = string
-  default     = "10.0.2.0/24"
+  default     = ["10.0.2.0/24"]
 }
