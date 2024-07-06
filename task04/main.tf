808962ddfaefef4c107771697efe6aeadc4b997f
gorgevorgyan253 <166944768+gorgevorgyan253@users.noreply.github.com>
2024-07-06 12:36:08 +0400

    NSG

--- task04/main.tf
+++ task04/main.tf
@@ -29,30 +29,34 @@ resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
 
-  security_rule {
-    name                       = var.nsg_rule_ssh
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+resource "azurerm_network_security_rule" "name" {
+  network_security_group_name = azurerm_network_security_group.nsg
+  resource_group_name = azurerm_resource_group.rg
+  name = "allow-http"
+  priority = 1002
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "TCP"
+  source_port_range = "*"
+  destination_port_range = "80"
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+}
 
-  security_rule {
-    name                       = "allow-http"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+resource "azurerm_network_security_rule" "name" {
+  network_security_group_name = azurerm_network_security_group.nsg
+  resource_group_name = azurerm_resource_group.rg
+  name = "allow-ssh"
+  priority = 1001
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "TCP"
+  source_port_range = "*"
+  destination_port_range = "22"
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
 }
 
 resource "azurerm_public_ip" "public_ip" {
