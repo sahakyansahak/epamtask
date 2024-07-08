8b20162b5c8f5555c4a97701f148ed2a07288e7b
Sahak <68821444+sahakyansahak@users.noreply.github.com>
2024-07-08 10:20:35 +0400

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = var.resource-group-name
+  name     = "${var.resource-group-name}"
   location = var.location
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = var.storage-account-name
+  name                     = "${var.storage-account-name}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = var.account-tier
@@ -16,21 +16,21 @@ resource "azurerm_storage_account" "main" {
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = var.vnet-name
+  name                = "${var.vnet-name}"
   address_space       = var.vnet-address
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "public" {
-  name                 = var.public-subnet-name
+  name                 = "${var.public-subnet-name}"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = var.subnet1-address
 }
 
 resource "azurerm_subnet" "private" {
-  name                 = var.private-subnet-name
+  name                 = "${var.private-subnet-name}"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = var.subnet2-address
